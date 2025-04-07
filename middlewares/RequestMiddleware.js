67de8af444a2dd72898981e7ef28a188bc1b39fe
Efe Murat Rendeci <eferendeci@gmail.com>
2025-04-07 17:19:12 +0300

    fix: removed unnecessary codes

--- middlewares/RequestMiddleware.js
+++ middlewares/RequestMiddleware.js
@@ -1,15 +1,10 @@
-import cyrpto from "crypto";
 import namespace from "../helpers/CLSHelper.js";
-
-const generateUUID = () => {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
-        (c ^ cyrpto.randomBytes(1)[0] & 15 >> c / 4).toString(16));
-}
+import StringHelper from "../../config-api/helpers/StringHelper.js";
 
 export default new class LogMiddleware {
     setTraceId = async (req, res, next) => {
         namespace.run(_ => {
-            const traceId = generateUUID();
+            const traceId = StringHelper.generateUUID();
 
             namespace.set('traceId', traceId);
             req.traceId = traceId;
