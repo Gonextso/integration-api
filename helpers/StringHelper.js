83654df76c3922437698990c9904ddfe3753e719
Efe Murat Rendeci <eferendeci@gmail.com>
2025-04-07 17:16:42 +0300

    feat: generateUUID added to string helper

--- helpers/StringHelper.js
+++ helpers/StringHelper.js
@@ -19,4 +19,9 @@ export default class StringHelper {
       .normalize("NFD")
       .replace(/[\u0300-\u036f]/g, "")
   }
+
+  static generateUUID = _ => {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+      (c ^ cyrpto.randomBytes(1)[0] & 15 >> c / 4).toString(16));
+  }
 }
