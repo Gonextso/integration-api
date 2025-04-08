2eacef49c2a6cd42ca93911777467488a8895a24
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-04-08 15:07:29 +0300

    fix: imports

--- app.config.js
+++ app.config.js
@@ -1,6 +1,6 @@
 export default {
     additionalBuilders: [
-        "/builders/cache/{cache_builder_placeholder}.js",
-        "/builders/database/{database_builder_placeholder}.js"
+        "/builders/cache/redisBuilder.js",
+        "/builders/database/mongooseBuilder.js"
     ]
 } //* you can remove additionalBuilders values if no need
