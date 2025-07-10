964358a3deecce27979d02ac9007d53ca4c8ece8
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-11 02:08:05 +0300

    traceId changed

--- middlewares/RequestMiddleware.js
+++ middlewares/RequestMiddleware.js
@@ -4,7 +4,7 @@ import StringHelper from "../helpers/StringHelper.js";
 export default new class RequestMiddleware {
     setTraceId = async (req, res, next) => {
         namespace.run(_ => {
-            const traceId = StringHelper.generateUUID();
+            const traceId = StringHelper.generateUUID().replace(/-/g, "");
 
             namespace.set('traceId', traceId);
             req.traceId = traceId;
