dddec7329bb887b76f0f66796b1f35d3c9ebd816
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-28 20:38:56 +0300

    revert this testing

--- models/db/Tenant.js
+++ models/db/Tenant.js
@@ -67,11 +67,11 @@ export default mongoose.model('Tenant', new mongoose.Schema({
                         isActive: { type: Boolean, default: false }
                     },
                     details: {
-                        interval: { type: String, default: "0 0 * * *" },
+                        interval: { type: String, default: "*/5 * * * *" },
                         startDate: {
                             type: String,
                             default: function () {
-                                const interval = moment.duration(1, "days");
+                                const interval = moment.duration(5, "minutes");
                                 return moment().subtract(interval).toISOString();
                             }
                         },
@@ -80,22 +80,22 @@ export default mongoose.model('Tenant', new mongoose.Schema({
                 },
                 order: {
                     create_and_cancel: {
-                        interval: { type: String, default: "*/30 * * * *" },
+                        interval: { type: String, default: "*/5 * * * *" },
                         startDate: {
                             type: String,
                             default: function () {
-                                const interval = moment.duration(30, "minutes");
+                                const interval = moment.duration(5, "minutes");
                                 return moment().subtract(interval).toISOString();
                             }
                         },
                         isActive: { type: Boolean, default: false }
                     },
                     status: {
-                        interval: { type: String, default: "0 0 * * *" },
+                        interval: { type: String, default: "*/5 * * * *"  },
                         startDate: {
                             type: String,
                             default: function () {
-                                const interval = moment.duration(1, "days");
+                                const interval = moment.duration(5, "minutes");
                                 return moment().subtract(interval).toISOString();
                             }
                         },
@@ -105,11 +105,11 @@ export default mongoose.model('Tenant', new mongoose.Schema({
             },
             redention: {
                 logs: {
-                    interval: { type: String, default: "0 0 * * 7" },
+                    interval: { type: String, default: "*/5 * * * *"  },
                     startDate: {
                         type: String,
                         default: function () {
-                            const interval = moment.duration(7, "days");
+                            const interval = moment.duration(5, "minutes");
                             return moment().subtract(interval).toISOString();
                         }
                     },
